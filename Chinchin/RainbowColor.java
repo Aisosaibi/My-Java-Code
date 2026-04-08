79f7053260708cf88f815b6daa2a7f7e0b087c2b
Aisosaibi <Aisosaibi.w@gmail.com>
2026-04-08 04:40:03 +0100

    Upload to Chinchin

---
+++ Chinchin/RainbowColor.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class RainbowColor {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int number = random.nextInt(7) + 1;
+
+        if (number == 1) {
+            System.out.println("Violet");
+        } else if (number == 2) {
+            System.out.println("Indigo");
+        } else if (number == 3) {
+            System.out.println("Blue");
+        } else if (number == 4) {
+            System.out.println("Green");
+        } else if (number == 5) {
+            System.out.println("Yellow");
+        } else if (number == 6) {
+            System.out.println("Orange");
+        } else {
+            System.out.println("Red");
+        }
+    }
+}
